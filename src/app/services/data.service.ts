7fabcb3c8c9c1605a21387d86f3068de4a9b0b9f
14gg3r <148910154+14gg3r@users.noreply.github.com>
2024-01-20 13:34:14 +0300

    initial commit

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -14,61 +14,26 @@ export interface Message {
 export class DataService {
   public messages: Message[] = [
     {
-      fromName: 'Matt Chorsey',
-      subject: 'New event: Trip to Vegas',
-      date: '9:32 AM',
+      fromName: 'Драйв',
+      subject: '52р',
+      date: '',
       id: 0,
       read: false
     },
     {
-      fromName: 'Lauren Ruthford',
-      subject: 'Long time no chat',
-      date: '6:12 AM',
+      fromName: 'Бёрн',
+      subject: '152Р',
+      date: '',
       id: 1,
       read: false
     },
     {
-      fromName: 'Jordan Firth',
-      subject: 'Report Results',
-      date: '4:55 AM',
+      fromName: 'Редбул',
+      subject: '252Р',
+      date: '',
       id: 2,
       read: false
     },
-    {
-      fromName: 'Bill Thomas',
-      subject: 'The situation',
-      date: 'Yesterday',
-      id: 3,
-      read: false
-    },
-    {
-      fromName: 'Joanne Pollan',
-      subject: 'Updated invitation: Swim lessons',
-      date: 'Yesterday',
-      id: 4,
-      read: false
-    },
-    {
-      fromName: 'Andrea Cornerston',
-      subject: 'Last minute ask',
-      date: 'Yesterday',
-      id: 5,
-      read: false
-    },
-    {
-      fromName: 'Moe Chamont',
-      subject: 'Family Calendar - Version 1',
-      date: 'Last Week',
-      id: 6,
-      read: false
-    },
-    {
-      fromName: 'Kelly Richardson',
-      subject: 'Placeholder Headhots',
-      date: 'Last Week',
-      id: 7,
-      read: false
-    }
   ];
 
   constructor() { }
